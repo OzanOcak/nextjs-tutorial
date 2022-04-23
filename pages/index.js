ab27989d177082972c1119c9243aefdded7c355b
OzanOcak <ozanocak@gmail.com>
2022-04-22 22:40:13 -0400

    displaying props

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -19,7 +21,23 @@ export default function Home() {
       <Head>
         <title>Pokemon</title>
       </Head>
-      <div>{JSON.stringify(pokemon)}</div>
+      <div className={styles.grid}>
+        {pokemon.map((pokemon) => (
+          <div className={styles.card} key={pokemon.id}>
+            <Link href={`/pokemon/${pokemon.id}`}>
+              <a>
+                <img
+                  src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+                  alt={pokemon.name}
+                  height={90}
+                  width={80}
+                />
+                <h3>{pokemon.name}</h3>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
